361c1e21c487dd66af6ccf350619610f810b1104
Hugh MacNally <hugh.macnally@codewizards.co.uk>
2024-10-03 16:13:23 +0100

    Added logging function, and ServeMux.

--- main.go
+++ main.go
@@ -7,15 +7,28 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	switch r.Method {
+	case "GET":
+		fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":8080", nil)
+	portNum := 8080
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+	logger("Starting server on port number: %d", portNum)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", portNum), nil)
 	if err != nil {
-		errMessage := fmt.Sprintf("Error starting server: %s", err)
-		log.Println(errMessage)
+		logger("Error starting on port number: %d", portNum)
 		return
 	}
+}
+
+func logger(format string, a ...any) {
+	log.Println(fmt.Sprintf(format, a...))
 }
