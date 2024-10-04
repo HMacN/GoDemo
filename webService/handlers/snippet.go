43984e9947f04f87ea3570b40bfdfe7dced5bf68
Hugh MacNally <hugh.macnally@codewizards.co.uk>
2024-10-04 16:45:31 +0100

    Added a simple test for URL parameters.

--- webService/handlers/snippet.go
+++ webService/handlers/snippet.go
@@ -1,7 +1,13 @@
 ﻿package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+)
 
 func Snippet(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
 
+	w.Header().Set("Id", strconv.Itoa(id))
+	w.WriteHeader(http.StatusOK)
 }
--- webService/handlers/snippet_test.go
+++ webService/handlers/snippet_test.go
@@ -4,17 +4,21 @@ import (
 	"GoDemo/assert"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
 func TestSnippet_ReturnsSnippetId(t *testing.T) {
+	testId := 1
 	responseRecorder := httptest.NewRecorder()
-
-	request, err := http.NewRequest(http.MethodGet, "/view?id=1", nil)
+	request, err := http.NewRequest(http.MethodGet, "/?id="+strconv.Itoa(testId), nil)
 	if err != nil {
 		assert.Fail(t, err.Error())
 		return
 	}
 
 	Snippet(responseRecorder, request)
+	result := responseRecorder.Result().Header.Get("Id")
+
+	assert.Equal(t, strconv.Itoa(testId), result)
 }
