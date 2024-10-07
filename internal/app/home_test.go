b921661a53fc14fc7528daee3da0915e740e3a69
Hugh MacNally <hugh.macnally@codewizards.co.uk>
2024-10-07 11:32:19 +0100

    Added test to ensure 'Home' handles unauthorised URLs.

--- internal/app/home_test.go
+++ internal/app/home_test.go
@@ -1,7 +1,27 @@
 ﻿package app
 
-import "testing"
+import (
+	"GoDemo/internal/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestHome(t *testing.T) {
 
+}
+
+func TestApplicationHome_CatchAll(t *testing.T) {
+	responseRecorder := httptest.NewRecorder()
+	request, err := http.NewRequest(http.MethodGet, "/unauthorised/url", nil)
+	if err != nil {
+		assert.Fail(t, err.Error())
+		return
+	}
+
+	app := NewApp()
+	app.Home(responseRecorder, request)
+	result := responseRecorder.Result()
+
+	assert.Equal(t, http.StatusNotFound, result.StatusCode)
 }
