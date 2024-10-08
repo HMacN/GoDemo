cf80561cf8e4d8f7d7f5bb557be5aab566d10726
Hugh MacNally <hugh.macnally@codewizards.co.uk>
2024-10-08 12:37:13 +0100

    Added check that directory traveral attacks for the 'static' part of the app are guarded against.

--- internal/app/static_test.go
+++ internal/app/static_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestStatic(t *testing.T) {
+func TestApplicationStatic_ServesFile(t *testing.T) {
 	testUrl := "http://localhost:2000/static/testing/testing.txt"
 	app := NewApp()
 	mux := *app.Routes()
@@ -29,5 +29,23 @@ func TestStatic(t *testing.T) {
 	expected := string(testFileContent)
 	mux.ServeHTTP(responseRecorder, request)
 	result := responseRecorder.Body.String()
+	assert.Equal(t, expected, result)
+}
+
+func TestApplicationStatic_StopsDirectoryTraversal(t *testing.T) {
+	testUrl := "http://localhost:2000/static/../.."
+	app := NewApp()
+	mux := *app.Routes()
+
+	responseRecorder := httptest.NewRecorder()
+	request, err := http.NewRequest(http.MethodGet, testUrl, nil)
+	if err != nil {
+		assert.Fail(t, err.Error())
+		return
+	}
+
+	mux.ServeHTTP(responseRecorder, request)
+	result := responseRecorder.Result().StatusCode
+	expected := http.StatusMovedPermanently
 	assert.Equal(t, expected, result)
 }
